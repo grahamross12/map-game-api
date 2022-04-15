2afdfaf36df742726f6c4693741bcfc1da119e3b
EC2 Default User <ec2-user@ip-172-31-34-0.eu-west-2.compute.internal>
2022-04-15 09:14:51 +0000

    Added API to read daily numbers file

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Successful response.');
+const DAILY_NUMBERS_FILE = "/home/ec2-user/shared/daily_numbers.json";
+app.get('/api/daily-numbers', (req, res) => {
+  fs.readFile(DAILY_NUMBERS_FILE, (err, data) => {
+    let countries;
+    if (err) {
+      countries = [];
+      throw err;
+    };
+    countries = JSON.parse(data);
+  });
+  res.send(countries);
 });
 
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+app.listen(3000, () => console.log('API is listening on port 3000.'));
 
